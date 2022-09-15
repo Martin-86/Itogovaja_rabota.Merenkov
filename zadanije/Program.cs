99e49f6cbe386fb984e9ef0100a873e253f24263
Martin <rauch83@yandex.ru>
2022-09-16 02:08:34 +0300

    method added

--- zadanije/Program.cs
+++ zadanije/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+
+int Method(int count)
+{
+    int arrayindex = 0;
+    int result = 0;
+    while (arrayindex < count)
+    {
+        if (array[arrayindex].Length <= 3)           
+        {
+            result++;
+        }
+        arrayindex++;
+    }
+    return result;
+
+}
+
+int res = Method(array.Length);
+
+void FillArray(string[] elemetsless4)
+{
+    int count = array.Length;
+    int arrayindex = 0;
+    int elementless4index = 0;
+    while(arrayindex < count)
+    {
+        if (array[arrayindex].Length <= 3)           
+        {
+            elemetsless4[elementless4index] = array[arrayindex];
+            elementless4index++;
+            arrayindex++;
+        }
+        else
+        {
+            arrayindex++;
+        }
+        
+    }
+}
+
+void PrintArray(string[] elementless4)
+{
+    int elementless4index = 0;
+    while(elementless4index < res)
+    {
+        Console.Write(elementless4[elementless4index] + "  ");
+        elementless4index++;
+    }
+}
+
+
+string[] elemetsless4 = new string[res];
+FillArray(elemetsless4);
+PrintArray(elemetsless4);
